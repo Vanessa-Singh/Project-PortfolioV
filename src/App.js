fb07f4edbee4309313cfcabdfedf6a4de3451339
VSingh <vsingh@student.fullsail.edu>
2019-12-04 11:01:57 +0530

    Created additional components.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import "./App.css";
 
 //Components
 import Headline from "./components/headline/Headline";
+import Search from "./components/search/Search";
+import Weather from "./components/weather/Weather";
 class App extends Component {
   render(){
     return (
       <div>
         <Headline pgTitle="Weather Outlook" />
+        <Search />
+        <Weather />
       </div>
     );
   }
