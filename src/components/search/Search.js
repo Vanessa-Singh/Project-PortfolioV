d8b9f1ad8e7a26a8ce7ddc9704555aa41878ab60
VSingh <vsingh@student.fullsail.edu>
2019-12-05 10:22:38 +0530

    Created function to obtain user's input for city and country.

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -3,10 +3,22 @@ import Button from "../button/Button";
 
 const Search = props => {
     return (
-        //Set a React attribute to call the get_weather function.
+      //Set a React attribute to call the get_weather function.
       <form onSubmit={props.get_weather}>
-        <input type="text" name="city" placeholder="City" />
-        <input type="text" name="country" placeholder="Country" />
+        <input
+          type="text"
+          name="city"
+          placeholder="City"
+          onChange={props.takecity}
+          value={props.city}
+        />
+        <input
+          type="text"
+          name="country"
+          placeholder="Country"
+          onChange={props.takecountry}
+          value={props.country}
+        />
         <Button type="submit" btnText="Search" />
       </form>
     );
