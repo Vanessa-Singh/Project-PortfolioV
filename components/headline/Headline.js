8750b0431a8aa9aba4a9eec2d4930a4a58a6cc98
VSingh <vsingh@student.fullsail.edu>
2019-12-31 22:37:26 +0530

    Updates

--- components/headline/Headline.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./Headline.css";
-
-const Headline = props => {
-  return <div className="headlineContainer" key={props.id}>
-  {props.pgTitle && <h1>{props.pgTitle}</h1>}
-  </div>;
-};
-export default Headline;
---
+++ precache-manifest.7560e05310531ac102ba4c00d2e60e54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c52c567f74a27c79b24ae8b5f7709237",
+    "url": "/Project-PortfolioV/index.html"
+  },
+  {
+    "revision": "d2ac62c18378cd2190b2",
+    "url": "/Project-PortfolioV/static/css/main.17dc2f24.chunk.css"
+  },
+  {
+    "revision": "5163cdec6116d668d17d",
+    "url": "/Project-PortfolioV/static/js/2.37da85a9.chunk.js"
+  },
+  {
+    "revision": "d2ac62c18378cd2190b2",
+    "url": "/Project-PortfolioV/static/js/main.a0922ef6.chunk.js"
+  },
+  {
+    "revision": "f5d1d60048186894a076",
+    "url": "/Project-PortfolioV/static/js/runtime-main.858975c8.js"
+  },
+  {
+    "revision": "cf7785aa9147d2d46e47f8ad1f5f3381",
+    "url": "/Project-PortfolioV/static/media/weatherBkg.cf7785aa.png"
+  }
+]);
